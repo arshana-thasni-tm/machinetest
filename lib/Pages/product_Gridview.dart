d5ef864638958f0c180906ec19310e44f1cc1aef
Arshana Thasni TM <148333016+arshana-thasni-tm@users.noreply.github.com>
2024-07-30 16:53:36 +0530

    initial

--- lib/Pages/product_Gridview.dart
+++ lib/Pages/product_Gridview.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:machinetest/Pages/productDetailPages.dart';
 
-import '../controller/productController.dart';
-
 
 class GridPage extends StatelessWidget {
-  final ProductController productController = Get.put(ProductController());
+  // Static list of image URLs
+  final List<String> imageUrls = List.generate(
+    20,
+        (index) => 'https://picsum.photos/200/300?random=$index',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -15,46 +17,38 @@ class GridPage extends StatelessWidget {
         // Remove the title from the AppBar
         title: SizedBox.shrink(),
       ),
-      body: Obx(() {
-        if (productController.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
-        } else if (productController.products.isEmpty) {
-          return Center(child: Text('No products found'));
-        } else {
-          return GridView.builder(
-            padding: EdgeInsets.all(8.0),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // Number of columns
-              crossAxisSpacing: 8.0, // Space between columns
-              mainAxisSpacing: 8.0, // Space between rows
-              childAspectRatio: 1.0, // Aspect ratio of each grid item
-            ),
-            itemCount: productController.products.length,
-            itemBuilder: (context, index) {
-              final product = productController.products[index];
-              return GestureDetector(
-                onTap: () {
-                  Get.to(() => ProductDetailPage(
-                    imageUrl: 'https://mansharcart.com/image/${product.imageUrl}',
-                  ));
-                },
-                child: Card(
-                  elevation: 4.0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0), // Rounded corners
-                    child: Image.network(
-                      'https://mansharcart.com/image/${product.imageUrl}',
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                  ),
-                ),
-              );
+      body: GridView.builder(
+        padding: EdgeInsets.all(8.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Number of columns
+          crossAxisSpacing: 8.0, // Space between columns
+          mainAxisSpacing: 8.0, // Space between rows
+          childAspectRatio: 1.0, // Aspect ratio of each grid item
+        ),
+        itemCount: imageUrls.length,
+        itemBuilder: (context, index) {
+          final imageUrl = imageUrls[index];
+          return GestureDetector(
+            onTap: () {
+              Get.to(() => ProductDetailPage(
+                imageUrl: imageUrl,
+              ));
             },
+            child: Card(
+              elevation: 4.0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                child: Image.network(
+                  imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+              ),
+            ),
           );
-        }
-      }),
+        },
+      ),
     );
   }
 }
